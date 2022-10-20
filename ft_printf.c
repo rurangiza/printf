428a5a43c287a5733800cc426984349f660f661f
Arsene Rurangiza <arurangi@mi-c2-p2.s19.be>
2022-10-20 17:32:54 +0200

    updated

--- ft_printf.c
+++ ft_printf.c
@@ -1,13 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.c                                           :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:49:26 by arurangi          #+#    #+#             */
-/*   Updated: 2022/10/20 16:49:39 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/10/20 17:32:34 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...);
+#include "libftprintf.h"
+#include <stdio.h>
+
+// va_start, va_arg, va_end
+
+int	ft_printf(const char *, ...)
+{
+	// if %c, replace with char variable
+	// if %s, replace with string variable 
+	// if %p, replace with 
+	va_list args;
+
+	va_start(args, *);
+}
+// What are variadic functions?
+// How can I print a single character -> {%c}
+
+int main(void)
+{ 
+	//ft_printf("", a, h);
+	int a = 42;
+	int *ptr = &a;
+	printf("%p\n", ptr);
+	return (0);
+}
